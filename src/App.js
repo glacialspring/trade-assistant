9aa9f8582f76cabf1572f1999d926a71a533b852
Richard Foo <foo.richard@gmail.com>
2020-09-26 14:20:32 -0400

    draw stops and targets

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react'
 import Chart from './components/chart/chart'
 import candles from './components/chart/TSLA_short'
 
-function App() {
+const App = () => {
   return (
     <div style={{ display: 'flex', justifyContent: 'center' }}>
       <div style={{ borderColor: '#2a2e39', borderWidth: 3, borderStyle: 'solid', margin: 20 }}>
-        <Chart candles={ candles } />
+        <Chart candles={ candles } width={1000} height={500} />
       </div>
     </div>
   )
--- src/components/chart/chart.js
+++ src/components/chart/chart.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useState, useLayoutEffect, useRef } from 'react'
 
+const lime = '#66bb6a'
+const green = '#26a69a'
+const red = '#ef5350'
+const gray = '#758696'
+const orange = '#f57c00'
 
 const Chart = props => {
-  const { candles } = props
+  const {
+    candles,
+    width: canvasW,
+    height: canvasY,
+  } = props
 
+  // touch
+  const containerRef = useRef()
+  const origin = useRef()
+  useLayoutEffect(() => {
+    const { x, y } = containerRef.current.getBoundingClientRect()
+    origin.current = { x,y }
+  })
+  const [cursor, setCursor] = useState()
+  const onMouseMove = e => {
+    setCursor({ x: e.clientX - origin.current.x, y: e.clientY - origin.current.y })
+    console.log(cursor)
+  }
+  const onMouseLeave = () => {
+    setCursor(undefined)
+  }
+
+  // stops
+  const [stops, setStops] = useState([])
+  const onPlaceStop = e => {
+    const y = e.clientY - origin.current.y
+    const stop = -(y + margin - canvasY) / drawableH * height
+    setStops([...stops, stop])
+  }
+  // targets
+  const [targets, setTargets] = useState([])
+  const onPlaceTarget = e => {
+    const y = e.clientY - origin.current.y
+    const target = -(y + margin - canvasY) / drawableH * height
+    setTargets([...targets, target])
+  }
+
+  // draw
   let min = 1000000000
   let max = -1
   for (const cd of candles) {
@@ -17,8 +58,6 @@ const Chart = props => {
   const startTs = candles[0].datetime
   const endTs = candles[candles.length-1].datetime
 
-  const canvasW = 1000
-  const canvasY = 500
   const margin = 10
   const drawableW = canvasW-2*margin
   const drawableH = canvasY-2*margin
@@ -27,35 +66,61 @@ const Chart = props => {
   const height = max - min
 
   return (
-    <svg height={ canvasY } width={ canvasW }>
-      { candles.map(cd => {
-        const {
-          open: o,
-          close: c,
-          low: l,
-          high: h,
-          datetime: ts,
-        } = cd
-        const x = (ts - startTs) / width * drawableW + margin
-        const yo = canvasY - (o - min)/height * drawableH - margin
-        const yc = canvasY - (c - min)/height * drawableH - margin
-        const yl = canvasY - (l - min)/height * drawableH - margin
-        const yh = canvasY - (h - min)/height * drawableH - margin
-        const candleW_2 = candleWidth / 2
-        const yt = Math.min(yo, yc)
-        const yb = Math.max(yo, yc)
-        const color = c > o ? '#26a69a' : '#ef5350'
+    <div
+      ref={ containerRef }
+      onMouseMove={ onMouseMove }
+      onMouseLeave={ onMouseLeave }
+      onClick={ onPlaceTarget }
+    >
+      <svg height={ canvasY } width={ canvasW }>
+        { candles.map(cd => {
+          const {
+            open: o,
+            close: c,
+            low: l,
+            high: h,
+            datetime: ts,
+          } = cd
+          const x = (ts - startTs) / width * drawableW + margin
+          const yo = canvasY - (o - min)/height * drawableH - margin
+          const yc = canvasY - (c - min)/height * drawableH - margin
+          const yl = canvasY - (l - min)/height * drawableH - margin
+          const yh = canvasY - (h - min)/height * drawableH - margin
+          const candleW_2 = candleWidth / 2
+          const yt = Math.min(yo, yc)
+          const yb = Math.max(yo, yc)
+          const color = c > o ? green : red
 
-        return (
-          <polygon
-            points={`${x-candleW_2},${yt} ${x},${yt} ${x},${yh} ${x},${yt} ${x+candleW_2},${yt} ${x+candleW_2},${yb} ${x},${yb} ${x},${yl} ${x},${yb} ${x-candleW_2},${yb}`}
-            fill={ color }
-            stroke={ color }
-            stroke-width="1"
-          />
-        )})
-      }
-    </svg>
+          return (
+            <polygon
+              points={`${x-candleW_2},${yt} ${x},${yt} ${x},${yh} ${x},${yt} ${x+candleW_2},${yt} ${x+candleW_2},${yb} ${x},${yb} ${x},${yl} ${x},${yb} ${x-candleW_2},${yb}`}
+              fill={ color }
+              stroke={ color }
+              stroke-width="1"
+            />
+          )})
+        }
+        { cursor &&
+          <>
+            <line x1="0" y1={ cursor.y } x2={ canvasW } y2={ cursor.y } stroke={ gray } stroke-width="1" stroke-dasharray="5,6" />
+          </>
+        }
+        { stops.map( stop => {
+            const y = canvasY - stop/height * drawableH - margin
+            return (
+              <line x1="0" y1={ y } x2={ canvasW } y2={ y } stroke={ orange } stroke-width="2" stroke-dasharray="2,6" />
+            )
+          })
+        }
+        { targets.map( target => {
+            const y = canvasY - target/height * drawableH - margin
+            return (
+              <line x1="0" y1={ y } x2={ canvasW } y2={ y } stroke={ lime } stroke-width="2" stroke-dasharray="2,6" />
+            )
+          })
+        }
+      </svg>
+    </div>
   )
 }
 
